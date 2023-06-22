73fd4af96ceb6e904872b2983964a82de85ebda4
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-06-22 23:52:06 +0300

    5

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -22,8 +22,14 @@
 	color="rgba(243,218,11,.4)" />
 <BlockTextImage
 	anchor="a4"
-	reverse
 	text="Оформите заказ"
 	description="Используйте банковскую карту"
-	image="https://www.iphones.ru/wp-content/uploads/2021/04/553E4B5E-7E16-42F6-B0A0-F20F08C3700F.jpeg"
-	color="rgba(128,0,255,.4)" />
+	image="https://zakonpravorf.ru/wp-content/uploads/2023/02/Kreditnaya-ekspress-karta.jpg"
+	color="rgba(4,123,225,.6)" />
+<BlockTextImage
+	reverse
+	anchor="a5"
+	text="Прикрепите к горизонтальной поверхности"
+	description="Опоре или потолку"
+	image="https://cs1.livemaster.ru/storage/9b/2f/abcd43bbadf392bbfb9c6fcda9od--dlya-doma-i-interera-podvesnoe-kreslo-gamak-dlya-doma-i-ulits.jpg"
+	color="rgba(90,100,11,.4)" />
