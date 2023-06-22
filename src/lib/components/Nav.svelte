e4e88154c7ebf865bfacea567cbda58388a5cab0
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-06-22 23:35:51 +0300

    4

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -17,7 +17,7 @@
 	</nav>
 	<nav
 		class="position-fixed bottom-0 w-100 d-flex flex-column align-items-center justify-content-between bg-light bg-opacity-10 gap-1 px-3 py-2">
-		<div class="d-flex align-items-center gap-1">
+		<div class="d-flex align-items-center gap-2">
 			{#each routes as item}
 				<a href="#{item.url}" class="btn btn-sm btn-light bg-light bg-opacity-50 border-0 text-dark">{item.text}</a>
 			{/each}
